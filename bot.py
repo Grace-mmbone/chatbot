fa064eb5e6fe6bd8dd5a7a696aad5c654ce3c2b4
Grace-mmbone <gracemmbonestd17@gmail.com>
2026-04-17 17:40:07 +0300

    my first chatbot files

---
+++ bot.py
@@ -0,0 +1,17 @@
+print("bot: hello! i am a simpel chatbot. type'quit' to exit")
+while True:
+    user-input=("you:").lower()
+    if"quit"in User-input or"exit" in user-input:
+    print("bot: goodbye")
+    break
+if "elif" in user-input or "hi" in user-input  or "hey" in user-input :
+    print('bot: hello there! how can i help you today')
+    
+elif "else" in user-input:
+    print("bot: i am just a code, but i am running perfectly!")
+    
+elif "hello" in user_input:
+    print("bot: you can ask about my name, how i am, or just say hello")
+    
+else:
+    print("bot i dont understand that. try 'hello' or'help'")
